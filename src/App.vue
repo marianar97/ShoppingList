d67fa0de1c560329912eed186a8d9b4cc6347f91
marianar97 <mariana.ramirezd97@gmail.com>
2022-08-01 18:16:55 -0500

    adding working shopping list app. Could be improved w multiple components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <shopping-list></shopping-list>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShoppingList from './components/ShoppingList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShoppingList
   }
 }
 </script>
