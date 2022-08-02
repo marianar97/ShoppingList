da35624e1a744bc3018b0ec8131fe1e052ebc8c3
marianar97 <mariana.ramirezd97@gmail.com>
2022-08-02 16:33:34 -0500

    modifying app to use two components

---
+++ src/components/ShoppingItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li class="list-group-item shop-item" v-bind:class="{ completed: completed }">
+    <input
+      class="form-check-input checkbox-item"
+      type="checkbox"
+      value=""
+      id="flexCheckDefault"
+      v-model="completed"
+    />
+    {{ item.name }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ShoppingItem",
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      completed: this.item.completed,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.shop-item {
+  text-align: left;
+}
+
+.checkbox-item {
+  margin-right: 10px;
+}
+
+.completed {
+  text-decoration: line-through;
+  color: gray;
+}
+</style>
--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,65 +1,53 @@
 <template>
-    <div class="container">
-        <h2>Shopping List</h2>
-        <div>
-            <input type="text" placeholder="Enter shopping item" class="form-control" v-on:keydown.enter="getItem" v-model="currentItem">
-            <ul class="list-group mt-1">
-                <li class="list-group-item shop-item" v-bind:class="{completed: item.completed}" v-for="item in list" v-bind:key="item.idItem">
-                    <input class="form-check-input checkbox-item" type="checkbox" value="" id="flexCheckDefault" v-model="item.completed">
-                    {{item.name}}
-                </li>
-            </ul>
-        </div>
+  <div class="container">
+    <h2>Shopping List</h2>
+    <div>
+      <input
+        type="text"
+        placeholder="Enter shopping item"
+        class="form-control"
+        v-on:keydown.enter="getItem"
+        v-model="currentItem"
+      />
+      <ul class="list-group mt-1">
+        <shopping-item
+          v-for="item in list"
+          v-bind:key="item.idItem"
+          :item="item"
+        ></shopping-item>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'ShoppingList',
-        data() {
-            return {
-                idItem: 2,
-                currentItem: '',
-                list: [
-                    {
-                        id: 1,
-                        name: 'banana',
-                        completed: false
-                    }
-                ]
-            }
-        },
-        methods: {
-            getItem(){
-                if (this.currentItem.trim().length == 0) {
-                    return
-                }
-                this.list.push({
-                    id: this.idItem,
-                    name: this.currentItem,
-                    completed: false 
-                })
-                this.currentItem = '';
-                this.idItem++;
-            }
-        }
-    }
+import ShoppingItem from "./ShoppingItem.vue";
+
+export default {
+  name: "ShoppingList",
+  components: {
+    ShoppingItem,
+  },
+  data() {
+    return {
+      idItem: 2,
+      currentItem: "",
+      list: [],
+    };
+  },
+  methods: {
+    getItem() {
+      if (this.currentItem.trim().length == 0) {
+        return;
+      }
+      this.list.push({
+        id: this.idItem,
+        name: this.currentItem,
+        completed: false,
+      });
+      this.currentItem = "";
+      this.idItem++;
+    },
+  },
+};
 </script>
-
-
-<style scoped>
-
-.shop-item {
-    text-align: left;
-}
-
-.checkbox-item{
-    margin-right: 10px;
-}
-
-.completed {
-    text-decoration: line-through;
-    color: gray;
-}
-
-</style>
